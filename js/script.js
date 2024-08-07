7043d0b330154e0e9b0894c81a78f4e46be3a576
Coutinh00 <lucasconsentino57@gmail.com>
2024-08-06 21:04:41 -0300

    INDEXOF, SPLICE e DELETE

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 //Declarar um array de frutas brasileiras exóticas
 let frutas = ["Açaí", "Guarana", "Cupuaçú","Jambo", "Jaca"];
 
@@ -85,8 +86,25 @@ document.getElementById('btnRemove').addEventListener('click',()=>{
     renderizaLista();
     });
 
+ //=====================================INDEXOF() e SPLICE()
+ document.getElementById('btnPesquisa').addEventListener('click',()=>{
+    let valorDigitadoNoCampo = document.getElementById('idFruta').value;
 
+    console.log(frutas.indexOf("Jaca"));
 
+ });
+
+ function deletaFruta(){
+    let valorDigitadoNoCampo = document.getElementById('idFruta').value;
+    let indice = frutas.indexOf(valorDigitadoNoCampo);
+    if(indice != -1){
+        frutas.splice(indice,1);
+        }
+        renderizaLista();
+ }
+
+ document.getElementById('btnDelete').addEventListener('click',deletaFruta);
+ 
 
 
 renderizaLista();
